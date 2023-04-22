cc09d26e2d01c68a10997c6da26f61ea63e5ee02
shubhu1026 <shubhu.p2000@gmail.com>
2023-04-22 14:53:50 +0530

    shopping cart finished

--- Shopping Cart/cart.cpp
+++ Shopping Cart/cart.cpp
@@ -1,13 +1,121 @@
 #include<iostream>
+#include<vector>
+#include<stdlib.h>
 #include "datamodel.h"
+
+vector<Product> allProducts =
+{
+    Product (1,"apple", 26),
+    Product (3,"mango", 16),
+    Product (2,"guava", 36),
+    Product (5,"banana", 56),
+    Product (4,"strawberry", 29),
+    Product (6,"pineapple", 20)
+};
+
+Product* chooseProduct()
+{
+    string productList;
+    cout<<endl<<"Available Products: "<<endl;
+
+    for(auto product : allProducts)
+    {
+        productList.append(product.getDisplayName());
+    }
+    cout<<productList;
+
+    cout<<"----------------------"<<endl;
+
+    string choice;
+    cin>>choice;
+
+    for(int i=0; i < allProducts.size(); i++)
+    {
+        if(allProducts[i].getShortName() == choice)
+        {
+            return &allProducts[i];
+        }
+    }
+
+    cout<<endl<<"Product not found"<<endl;
+    return NULL;
+}
+
+bool checkout(Cart &cart)
+{
+    if(cart.isEmpty())
+    {
+        return false;
+    }
+
+    int total = cart.getTotal();
+    cout<<"Pay in Cash Rs. "<<total<<endl ;
+
+    int paid;
+    cin>>paid;
+
+    if(paid>=total)
+    {
+        cout<<"Change "<<paid - total<<endl;
+        cout<<"Thank you for shopping!"<<endl;
+        return true;
+    }
+    else
+    {
+        cout<<"Not enough cash!"<<endl;
+        return false;
+    }
+}
 
 int main()
 {
-    Product p(1,"apple", 10);
-    cout<<p.getDisplayName();
+    char action;
+    Cart cart;
 
-    Item fruit(p, 3);
-    cout<<fruit.getItemInfo();
+    while(true)
+    {
+        system("cls");
+        cout<<endl;
+        cout<<"Select an action:"<<endl;
+        cout<<"1. Add item"<<endl;
+        cout<<"2. View cart"<<endl;
+        cout<<"3. Checkout"<<endl;
+
+        cin>>action;
+
+        if(action == '1')
+        {
+            //add to cart
+            //View all products + choose product + add to the cart
+            //system("cls");
+            Product* product = chooseProduct();
+            if(product != NULL)
+            {
+                cout<<"Added to the Cart "<<product->getDisplayName();
+                cart.addProduct(*product);
+            }
+        }
+        else if(action == '2')
+        {
+            //view cart
+            cout<<"----------------------"<<endl;
+            cout<<cart.viewCart();
+            cout<<"----------------------"<<endl;
+        }
+        else if(action == '3')
+        {
+            //checkout
+            cart.viewCart();
+            if(checkout(cart))
+            {
+                break;
+            }
+        }
+        else
+        {
+            cout<<endl<<"Invalid action. Please select your action again."<<endl;
+        }
+    }
 
     return 0;
 }
--- Shopping Cart/datamodel.h
+++ Shopping Cart/datamodel.h
@@ -1,4 +1,5 @@
 #include<string>
+#include<unordered_map>
 using namespace std;
 
 class Item;
@@ -10,6 +11,8 @@ class Product
     int price;
 
 public:
+    Product(){}
+
     Product(int u_id, string name, int price)
     {
         this->name = name;
@@ -22,15 +25,23 @@ public:
         return name + " : Rs " + to_string(price) + "\n";
     }
 
+    string getShortName()
+    {
+        return name.substr(0,1);
+    }
+
     friend class Item;
+    friend class Cart;
 };
 
 class Item
 {
-    const Product product;
+    Product product;
     int quantity;
 
 public:
+    Item(){}
+
     Item(Product p, int q): product(p), quantity(q){}
 
     int getItemPrice()
@@ -42,9 +53,62 @@ public:
     {
         return product.name + " x " + to_string(quantity) + " : Rs. " + to_string(quantity  * product.price);
     }
+
+    friend class Cart;
 };
 
 class Cart
 {
+    unordered_map<int, Item> items;
 
+public:
+    void addProduct(Product product)
+    {
+        if(items.count(product.id) == 0)
+        {
+            Item newItem(product,1);
+            items[product.id] = newItem;
+        }
+        else
+        {
+            items[product.id].quantity += 1;
+        }
+    }
+
+    int getTotal()
+    {
+        int total = 0;
+
+        for(auto itemPair : items)
+        {
+            auto item = itemPair.second;
+            total += item.getItemPrice();
+        }
+
+        return total;
+    }
+
+    string viewCart()
+    {
+        if(items.empty())
+        {
+            return "Cart is empty";
+        }
+
+        string itemizedList;
+        int cart_total = getTotal();
+
+        for(auto itemPair : items)
+        {
+            auto item = itemPair.second;
+            itemizedList.append(item.getItemInfo() + "\n");
+        }
+
+        return itemizedList + "\nTotal Amount : Rs " + to_string(cart_total) + "\n";
+    }
+
+    bool isEmpty()
+    {
+        return items.empty();
+    }
 };
